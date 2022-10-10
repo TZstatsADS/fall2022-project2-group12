b266adfca421d762fea079c1470a6fe7b4ce9654
Donglai Xu <60021258+xudonglai0426@users.noreply.github.com>
2022-10-09 20:41:56 -0400

    Add files via upload

---
+++ doc/donglai_code/server.R
@@ -0,0 +1,12 @@
+
+server <- function(input, output) {
+  output$tsPlot <-renderPlot({
+    ggplot(data=df_daily_case,aes_string(x=input$x, y=input$y))+
+      geom_bar(stat="identity",fill="#6baed6")+
+      theme(axis.text.x = element_text(angle=45,hjust=1), plot.title = element_text(hjust = 0.5))+
+      labs(title = "Covid-19 Statistics from 2020 to 2022")+
+      ylab("Count")+
+      scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y")
+    
+  })
+}
