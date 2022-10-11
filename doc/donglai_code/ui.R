5727dea016cf28cf47c3a05ea0820d9cf767f81a
Donglai Xu <60021258+xudonglai0426@users.noreply.github.com>
2022-10-11 18:09:57 -0400

    Update ui.R

--- doc/donglai_code/ui.R
+++ doc/donglai_code/ui.R
@@ -18,11 +18,11 @@ ui <- fluidPage(
       selectInput(inputId = "y",
                   label = "Choose a statistics:",
                   choices = c("Count of patients tested positive for Covid-19"="CASE_COUNT","Count of deaths among confirmed COVID-19 cases"="DEATH_COUNT","Count of COVID-19 patients who were hospitalized"="HOSPITALIZED_COUNT"),
-                  selected="CASE_COUNT"),
+                  selected="Count of patients tested positive for Covid-19"),
       selectInput(inputId = "x",
                   label = "Date",
                   choices = c("Date"="date_of_interest"),
-                  selected="date_of_interest"),
+                  selected="Date"),
     ),
     # Main panel for displaying output 
     mainPanel(
