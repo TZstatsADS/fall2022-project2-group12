6e71111d0adefa092adc47b97bd08db4647e23bb
YuliVivianYin <yuli.yin@columbia.edu>
2022-10-11 17:03:30 -0400

    Dataframe name modified

--- doc/yuli_code/global.R
+++ doc/yuli_code/global.R
@@ -25,34 +25,38 @@ water <- read.csv('../data/Water_Consumption_And_Cost__2013_-_Feb_2022_.csv')
 gas <- read.csv('../data/Heating_Gas_Consumption_And_Cost__2010_-__Feb_2022_.csv')
 
 # Select data from 2019-2022
-electric$Revenue.Year <- as.integer(substr(electric$Revenue.Month,1,4))
-water$Revenue.Year <- as.integer(substr(water$Revenue.Month,1,4))
-gas$Revenue.Year <- as.integer(substr(gas$Revenue.Month,1,4))
+electric2 <- electric
+water2 <- water
+gas2 <- gas
 
-electric <- electric[electric$Revenue.Year>=2019,]
-water <- water[water$Revenue.Year>=2019,]
-gas <- gas[gas$Revenue.Year>=2019,]
+electric2$Revenue.Year <- as.integer(substr(electric2$Revenue.Month,1,4))
+water2$Revenue.Year <- as.integer(substr(water2$Revenue.Month,1,4))
+gas2$Revenue.Year <- as.integer(substr(gas2$Revenue.Month,1,4))
+
+electric2 <- electric2[electric2$Revenue.Year>=2019,]
+water2 <- water2[water2$Revenue.Year>=2019,]
+gas2 <- gas2[gas2$Revenue.Year>=2019,]
 
 # Select key columns from original datasets
-electric <- electric[,c('Borough','Revenue.Month','Consumption..KWH.')]
-water <- water[,c('Borough','Revenue.Month','Consumption..HCF.')]
-gas <- gas[,c('Borough','Revenue.Month','Consumption..Therms.')]
+electric2 <- electric2[,c('Borough','Revenue.Month','Consumption..KWH.')]
+water2 <- water2[,c('Borough','Revenue.Month','Consumption..HCF.')]
+gas2 <- gas2[,c('Borough','Revenue.Month','Consumption..Therms.')]
 
 # Sort data
-electric <- electric[order(electric$Borough,electric$Revenue.Month),]
-water <- water[order(water$Borough,water$Revenue.Month),]
-gas <- gas[order(gas$Borough,gas$Revenue.Month),]
+electric2 <- electric2[order(electric2$Borough,electric2$Revenue.Month),]
+water2 <- water2[order(water2$Borough,water2$Revenue.Month),]
+gas2 <- gas2[order(gas2$Borough,gas2$Revenue.Month),]
 
 # Group by Borough and date, take average 
-electric_groupby <- electric %>% 
+electric_groupby <- electric2 %>% 
   group_by(Borough,Revenue.Month) %>% 
   summarise_at(vars(Consumption..KWH.), list(name = mean))
 
-water_groupby <- water %>% 
+water_groupby <- water2 %>% 
   group_by(Borough,Revenue.Month) %>% 
   summarise_at(vars(Consumption..HCF.), list(name = mean))
 
-gas_groupby <- gas %>% 
+gas_groupby <- gas2 %>% 
   group_by(Borough,Revenue.Month) %>% 
   summarise_at(vars(Consumption..Therms.), list(name = mean))
 
